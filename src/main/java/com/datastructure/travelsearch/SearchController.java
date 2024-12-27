316555a488477d33d4f6b0f77b6edb99f133b686
Fabian <Fabian@10.234.1.43>
2024-12-27 14:59:35 +0800

    Removes unused imports

--- src/main/java/com/datastructure/travelsearch/SearchController.java
+++ src/main/java/com/datastructure/travelsearch/SearchController.java
@@ -1,7 +1,6 @@
 package com.datastructure.travelsearch;
 
 import java.util.ArrayList;
-import java.util.PriorityQueue;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- src/main/java/com/datastructure/travelsearch/TreeBuilder.java
+++ src/main/java/com/datastructure/travelsearch/TreeBuilder.java
@@ -2,7 +2,6 @@ package com.datastructure.travelsearch;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class TreeBuilder {
